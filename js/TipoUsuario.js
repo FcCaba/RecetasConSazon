4cb9d837b60b91df021f12d1c08c496a03e11919
FcCaba <152334680+FcCaba@users.noreply.github.com>
2023-12-13 07:13:18 -0400

    Update TipoUsuario.js

--- js/TipoUsuario.js
+++ js/TipoUsuario.js
@@ -8,11 +8,13 @@ function Login() {
     let tipou = TipoInput.value;
     let password = passwordInput.value;
     if(gmail==""&& nombreu==""&& password=="" && tipou=="Administrativo"){
+        window.close()
         window.open("html/Usuario_register.html")
-        window.close()
+        
     }else if(gmail==""&& password=="" && tipou=="Inventario"){
-        window.open("html/inventario_Register.html")
         window.close()
+        window.open("html/login.html")
+        
     }
     else{
         if(gmail=="123@gmail.com"&& password=="franz"){
